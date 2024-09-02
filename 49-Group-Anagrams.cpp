1dfa52e9b6e16b536fe2b68bb0edd8dea28fc99e
moabo3li <mohmedali1462005@gmail.com>
2024-09-02 21:25:48 +0300

    [49] [Time Beats: 6.22%] [Memory Beats: 9.32%] - LeetPush

--- 49-Group-Anagrams.cpp
+++ 49-Group-Anagrams.cpp
@@ -2,15 +2,26 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>>mp;
-
-        for(auto x: strs){
-            string word =x;
-            sort(word.begin(),word.end());
-            mp[word].push_back(x);
-        }
         vector<vector<string>>ans;
-        for(auto x:mp)
-        ans.push_back(x.second);
+        if(strs.empty()) return ans;
+        for(auto st:strs)
+        {
+            vector<int> count(26,0);
+            for(char &c:st)
+            {
+                count[c-'a']++;
+            }
+            string key;
+            for(int &n:count)
+            {
+                key+="#"+to_string(n);
+            }
+            mp[key].push_back(st);
+        }
+        for(auto &i:mp)
+        {
+            ans.push_back(i.second);
+        }
         return ans;
     }
 };
