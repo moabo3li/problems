153483cc5ba23c22964bf043571ff13b1cec2e76
moabo3li <mohmedali1462005@gmail.com>
2024-09-03 20:21:46 +0300

    [238] [Time Beats: 82.61%] [Memory Beats: 83.17%] - LeetPush

--- 238-Product-of-Array-Except-Self.cpp
+++ 238-Product-of-Array-Except-Self.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int>ans(nums.size()),pre(nums.size()),post(nums.size());
-        pre[0]=1;
+        vector<int>ans(nums.size());
+        ans[0]=1;
         for(int i=1;i<nums.size();i++)
         {
-            pre[i]=pre[i-1]*nums[i-1];
+            ans[i]=ans[i-1]*nums[i-1];
         }
-        post[nums.size()-1]=1;
-        for(int i=nums.size()-2;i>=0;i--)
+        int post=1;
+        for(int i=nums.size()-1;i>=0;i--)
         {
-            post[i]=post[i+1]*nums[i+1];
-        }
-        for(int i=0;i<nums.size();i++)
-        {
-            ans[i]=pre[i]*post[i];
+            ans[i]*=post;
+            post*=nums[i];
         }
         return ans;
     }
